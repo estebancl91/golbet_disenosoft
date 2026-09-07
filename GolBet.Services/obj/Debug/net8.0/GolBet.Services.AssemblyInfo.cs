a41883345ac573c572d944ec15a52234c67d7aa3
Esteban Casas Léon <estebancl91@hotmail.com>
2026-09-07 06:55:51 -0500

    Add N-layer solution skeleton (Module 1)

--- GolBet.Services/obj/Debug/net8.0/GolBet.Services.AssemblyInfo.cs
+++ GolBet.Services/obj/Debug/net8.0/GolBet.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GolBet.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8fa22c88d7eb5735f22c0697b81b96cbd9127c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b6b64841ef26445b4e8f6402d0c422953b7457")]
 [assembly: System.Reflection.AssemblyProductAttribute("GolBet.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GolBet.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
