867e0fb63b4a52ffe1d1dbc06ccec345f41f7e35
Esteban Casas Léon <estebancl91@hotmail.com>
2026-09-17 20:47:57 -0500

    Add match detail page and board filters (Module 5)

--- GolBet.Services/Interfaces/IMatchService.cs
+++ GolBet.Services/Interfaces/IMatchService.cs
@@ -3,11 +3,16 @@ using GolBet.Entities.Enums;
 using GolBet.Services.DTOs;
  
 namespace GolBet.Services.Interfaces;
- 
+
+
+
+
 public interface IMatchService
 {
     /// <summary>Match board: all active matches ordered by date.</summary>
     Task<IEnumerable<MatchDto>> GetBoardAsync(MatchStatus? status = null);
+
+    Task<MatchDetailDto?> GetDetailAsync(int id);
 }
 
 
--- GolBet.Web/obj/Debug/net8.0/GolBet.Web.AssemblyInfo.cs
+++ GolBet.Web/obj/Debug/net8.0/GolBet.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GolBet.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b2818301afe2b71f94e86c30132ad384f7cfdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe58b735843b5cb9836298b80931bbec2af7b5b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GolBet.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GolBet.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
